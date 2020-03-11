7b7c577a62671b15d65a1560d694e4ed43a659dd
Suake-hub <59056593+Suake-hub@users.noreply.github.com>
2020-03-11 18:53:43 +0100

    Update onfaitduson.py

--- 20200311/onfaitduson.py
+++ 20200311/onfaitduson.py
@@ -2,7 +2,8 @@ import pygame
 from pygame.locals import *
 def bandeson(debut):
     if debut == 0:
-        musiquejeu = pygame.mixer.Sound("Sound/bandeson.wav")
-        pygame.mixer.music.load('Sound/bandeson.wav')
-        pygame.mixer.music.play(loops=-1)
-        pygame.mixer.music.set_volume(0.5)
+        debut = 0
+#        musiquejeu = pygame.mixer.Sound("Sound/bandeson.wav")
+#        pygame.mixer.music.load('Sound/bandeson.wav')
+#        pygame.mixer.music.play(loops=-1)
+#        pygame.mixer.music.set_volume(0.5)
